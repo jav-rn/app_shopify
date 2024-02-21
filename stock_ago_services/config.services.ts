e8b0a37946b4fa8375f27c924b59b52240221b51
jav-rn <develoterstock5@gmail.com>
2024-02-21 14:36:16 -0500

    update

--- stock_ago_services/config.services.ts
+++ stock_ago_services/config.services.ts
@@ -1,3 +1,4 @@
-export const config = {
-    order_create : "https://solucionesintegralesmallorca.com/portafoliojav/test_stock_ago/"
+export const routes = {
+    order_create : "https://solucionesintegralesmallorca.com/portafoliojav/test_stock_ago/", // OJO actualizar al endpoint de orders stockago
+    login : 'definir'
 }
