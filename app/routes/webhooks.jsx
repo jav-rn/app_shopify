37e142c3042a530461658e8ebf7e4c43f99044cb
jav-rn <develoterstock5@gmail.com>
2024-02-29 10:26:01 -0500

    se inicio la modificacion de payload para orders

--- app/routes/webhooks.jsx
+++ app/routes/webhooks.jsx
@@ -1,8 +1,34 @@
-import { authenticate } from "../shopify.server";
+import { authenticate, apiVersion } from "../shopify.server";
 import db from "../db.server";
 import { _DropiServices } from "../../stock_ago_services/dropi.services";
 // https://community.shopify.com/c/shopify-apps/you-do-not-have-permission-to-create-webhooks-with-orders-create/m-p/1919485
 
+
+const query = `
+query OrderQuery($orderId: ID!) {
+  order(id: $orderId) {
+    id
+    name
+    createdAt
+    lineItems(first: 250) {
+      edges {
+        node {
+          id
+          title
+          sku
+          variant {
+            product {
+              id
+              title
+           
+            }
+          }
+        }
+      }
+    }
+  }
+}
+`;
 
 const dropiServices = new _DropiServices();
 
@@ -23,9 +49,40 @@ export const action = async ({ request }) => {
       }
       break;
     case "ORDER_TRANSACTIONS_CREATE":
-      console.log("<-----se creo orden ORDER_TRANSACTIONS_CREATE------>")
-      console.log(payload)
-      dropiServices.SEND_ORDER_TRANSACTIONS_CREATE({ "webhook_ORDER_TRANSACTIONS_CREATE_body": payload });
+      console.log("<-----se creo orden ORDER_TRANSACTIONS_CREATE DDDDDDD------>");
+
+      // "gid://shopify/Order/5524261765318"
+      // const orderId = payload.id;  o payload.admin_graphql_api_id
+
+
+      const variables = { orderId: "gid://shopify/Order/"+payload.order_id };
+      console.log( "variables____>",variables);
+      const { shop, accessToken } = session
+      const response = await fetch(`https://${shop}/admin/api/${apiVersion}/graphql.json`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "X-Shopify-Access-Token": accessToken
+        },
+        body: JSON.stringify({
+          query,
+          variables
+        })
+      });
+      console.log("p2");
+
+      if (response.ok) {
+        console.log("p3");
+        const data = await response.json();
+        console.log("Respuesta de la consulta GraphQL:", data);
+        console.log(payload)
+        console.log("products_order", data)
+        dropiServices.SEND_ORDER_TRANSACTIONS_CREATE({ "webhook_ORDER_TRANSACTIONS_CREATE_body": payload, "products": data });
+        // Aquí puedes realizar las acciones necesarias con la información obtenida
+        // dropiServices.SEND_ORDER_TRANSACTIONS_CREATE(data);
+      }
+      console.log("p4");
+
       console.log("<-----end se creo orden ORDER_TRANSACTIONS_CREATE------>")
       break;
 
